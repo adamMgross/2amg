65b1f8e99a519772b7268036fdf4565de90d6a53
adammgross <amgross4@gmail.com>
2016-11-29 19:16:12 +0000

    Automated commit message.

--- ascii_fence2.py
+++ ascii_fence2.py
@@ -1,4 +1,13 @@
 x = ['|_/*\_|_/*\_|_/*\_|_/*\_|_/*\_|_/*\_|_/*\_|_/*\_|_/*\_|_/*\_|',
-'/*_|_\*/_|_\*/_|_\*/_|_\*/_|_\*/_|_\*/_|_\*/_|_\*/_|_\*/_|_\*']
 for i in range(12):
     print x[i % 2]
+     k;ljkhgjkl;'
+     ';lkjhgfhjkl;'
+     a;l;kljskhgfhajskl;d'
+     ;'l;k;jhgashdkjl;'
+     a's
+     l;d;kjhgjkl;';lk;jhgjkl;'
+     ;lk;jhg
+
+     a;lskjlhgjkl;'
+     lkjhg'
